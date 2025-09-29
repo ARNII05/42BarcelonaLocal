189a135e6d823de4c71c04c69573a93d98ef749d
Arnau <arnaubf05@gmail.com>
2025-09-29 15:54:53 +0200

    done

---
+++ Libft/ft_strchr.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arnau <arnau.bf05@>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/29 11:50:26 by arnau             #+#    #+#             */
+/*   Updated: 2025/09/29 11:50:26 by arnau            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int strlength(const char *c)
+{
+    int i;
+
+    i = 0;
+    while(c[i] != 0)
+        i++;
+    return (i);
+}
+
+char *strchr(const char *string, int c)
+{
+    int len;
+    int i;
+
+    i = 0;
+    len = strlength(string) + 1;
+    while (i < len)
+    {
+        if (string[i] == c)
+            return (char *) (string + i);
+        i++;
+    }
+    return (0);
+}
+
+int main() {
+
+    printf("%s\n", strchr("hola buenas tardes", 'u'));
+    return 0;
+}
