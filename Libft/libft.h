3739a45a3d800cf1e383c703c47c89391d96cc70
Arnau <arnaubf05@gmail.com>
2025-10-01 15:00:34 +0200

    to do

--- Libft/libft.h
+++ Libft/libft.h
@@ -39,6 +39,7 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 char    *ft_strjoin(char const *s1, char const *s2);
 char    *ft_strtrim(char const *s1, char const *set);
+char    **ft_split(char const *s, char c);
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
