80e64aade31297d3466b003fce514e72236eae58
Arnau <arnaubf05@gmail.com>
2025-10-21 15:36:11 +0200

    Add files via upload

--- Libft/ft_strmapi.c
+++ Libft/ft_strmapi.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
