2c72a7fc8ddc0faedac0beb86ac82a916af8a9a5
Arnau <arnaubf05@gmail.com>
2025-10-29 19:40:50 +0100

    start of program

--- get_next_line.c
+++ get_next_line.c
@@ -1,1 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arnau <arnau.bf05@>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/29 16:19:26 by arnau             #+#    #+#             */
+/*   Updated: 2025/10/21 10:50:43 by abellavi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "get_next_line.h"
+
+void readFileAndJoinResidue(int fd)
+{
+	static char *residue;
+	char buffer[BUFFER_SIZE + 1];
+	ssize_t n;
+
+	n = read(fd, buffer, BUFFER_SIZE);
+}
+
+char *get_next_line(int fd)
+{
+ char *actualLine;
+
+	readFileAndJoinResidue(fd);
+
+	return (actualLine);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -1,1 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arnau <arnau.bf05@>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/29 16:19:26 by arnau             #+#    #+#             */
+/*   Updated: 2025/10/21 10:50:43 by abellavi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 32
+#endif
+
+char *get_next_line(int fd);
+
+#endif
