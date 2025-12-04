5613c61f3ae0a4156a068d31c7e3125f0a38ad18
Arnau <arnaubf05@gmail.com>
2025-12-04 13:16:12 +0100

    Update ft_strjoin.c

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -19,13 +19,14 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	s2_len;
 
 	if (!s1 || !s2)
-		return (0);
+		return (NULL);
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
 	str = malloc(s1_len + s2_len + 1);
 	if (!str)
-		return (0);
-	ft_strlcpy(str, s1, s1_len + 1);
-	ft_strlcpy(str + s1_len, s2, s2_len + 1);
+		return (NULL);
+	memcpy(str, s1, s1_len);
+	memcpy(str + s1_len, s2, s2_len);
+	str[s1_len + s2_len] = '\0';
 	return (str);
 }
