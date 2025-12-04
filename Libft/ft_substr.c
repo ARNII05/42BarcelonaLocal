7617af0bed05f4d75f825d100f1b2a375b3f5422
Arnau <arnaubf05@gmail.com>
2025-12-04 13:50:05 +0100

    Update ft_substr.c

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -12,31 +12,22 @@
 
 #include "libft.h"
 
-#include "libft.h"
-#include <stdlib.h>
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+static char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
 	size_t	s_len;
-	size_t	i;
 
 	if (!s)
-		return (NULL);
+		return (0);
 	s_len = ft_strlen(s);
 	if (start >= s_len)
-		return (ft_strdup("")); 
+		return (ft_strdup(""));
 	if (len > s_len - start)
 		len = s_len - start;
 	sub = malloc(len + 1);
 	if (!sub)
-		return (NULL);
-	i = 0;
-	while (i < len)
-	{
-		sub[i] = s[start + i];
-		i++;
-	}
-	sub[i] = '\0';
+		return (0);
+	ft_memcpy(sub, s + start, len);
+	sub[len] = '\0';
 	return (sub);
 }
