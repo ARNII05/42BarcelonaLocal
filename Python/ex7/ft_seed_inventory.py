946593c7060debeee40fb9f2d8e7a36a01335648
Arnau <arnaubf05@gmail.com>
2026-01-20 22:24:44 +0100

    Update ft_seed_inventory.py

--- Python/ex7/ft_seed_inventory.py
+++ Python/ex7/ft_seed_inventory.py
@@ -1,10 +1,10 @@
 def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
-	if (unit != "packets" and unit != "area" and unit != "grams"):
+	if unit != "packets" and unit != "area" and unit != "grams":
 		print("Unknown unit type")
 		return
-	if (unit == "packets"):
+	if unit == "packets":
 		print(seed_type.capitalize(), "seeds:", quantity, "packets available")
-	elif (unit == "grams"):
+	elif unit == "grams":
 		print(seed_type.capitalize(), "seeds:", quantity, "grams total")
 	else:
 		print(seed_type.capitalize(), "seeds: covers", quantity, "square meters")
