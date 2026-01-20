b5986320c06cbb9ed4741d389f64f72bc28c25a7
Arnau <arnaubf05@gmail.com>
2026-01-20 19:31:37 +0100

    project done

---
+++ Python/ex5/ft_count_harvest_recursive.py
@@ -0,0 +1,11 @@
+def ft_count_harvest_recursive():
+	day = 0
+	total_days = int(input("Days until harvest: "))
+	main_recursion(day, total_days)
+
+def main_recursion(day : int, total_days: int):
+	if (total_days == day):
+		print("Harvest time!")
+		return 
+	print("Day", day + 1)
+	main_recursion(day + 1, total_days)
---
+++ Python/ex6/ft_garden_summary.py
@@ -0,0 +1,6 @@
+def ft_garden_summary():
+	garden_name = input("Enter garden name: ")
+	n_plants = input("Enter number of plants: ")
+	print("Garden:", garden_name)
+	print("Plants:", n_plants)
+	print("Status: Growing well!")
